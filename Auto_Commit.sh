317ec93acd1fb990ffb7899f4a154bc3864717de
priyansh19 <priyansh7533053411@gmail.com>
2019-08-23 19:11:07 +0530

    Test

---
+++ Auto_Commit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git add .
+
+echo 'Enter the commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+echo '1. Commit on master branch \n 2. Enter a new branch name to commit on'
+read branch
+if [$branch == "master"]
+then
+  git push origin $branch
+else
+  git checkout -b $branch
+  git push -u origin $branch
+fi
