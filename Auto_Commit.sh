4884a48ff1deac3e0d2a557197182e397d0839a6
Priyansh Gupta <priyansh7533053411@gmail.com>
2019-08-23 19:54:17 +0530

    Update Auto_Commit.sh

--- Auto_Commit.sh
+++ Auto_Commit.sh
@@ -7,13 +7,31 @@ read commitMessage
 
 git commit -m "$commitMessage"
 
-echo '1. Commit on master branch \n 2. Enter a new branch name to commit on'
-read branch
-if [$branch == "master"]
+echo '1. Press 1 to commit on master branch'
+echo '2. press 2 to commit on an existing branch [other than master]'
+echo '3. press 3 to enter a new branch name and commit in it'
+
+read x
+if [[ $x -eq 1]]
 then
-  git push origin $branch
-else
-  git checkout -b $branch
-  git push -u origin $branch
+  git push origin master
+
+elif [[ $x -eq 2 ]]
+then
+echo "Enter the name of an existing branch"
+read exist
+  git checkout $exist
+  git push -u origin $exist
   git checkout master
+
+elif [[ $x -eq 3 ]]
+then
+echo "Enter the name of a new branch"
+read new
+  git checkout -b $new
+  git push -u origin $new
+  git checkout master
+
+else
+ echo "You chose a wrong option.."
 fi
