0ca1dd3652b8139bff3dbd0cbcd8c53395b17b5b
priyansh19 <priyansh7533053411@gmail.com>
2019-08-23 20:24:41 +0530

    test

--- Auto_Commit.sh
+++ Auto_Commit.sh
@@ -11,27 +11,27 @@ echo '1. Press 1 to commit on master branch'
 echo '2. press 2 to commit on an existing branch [other than master]'
 echo '3. press 3 to enter a new branch name and commit in it'
 
-read x
-if [[ $x -eq 1]]
+read xterm
+if [ $xterm -eq '1']
 then
   git push origin master
 
-elif [[ $x -eq 2 ]]
+elif [ $xterm -eq 2'' ]
 then
-echo "Enter the name of an existing branch"
-read exist
+  echo "Enter the name of an existing branch"
+  read exist
   git checkout $exist
   git push -u origin $exist
   git checkout master
 
-elif [[ $x -eq 3 ]]
+elif [ $xterm -eq '3' ]
 then
-echo "Enter the name of a new branch"
-read new
+  echo "Enter the name of a new branch"
+  read new
   git checkout -b $new
   git push -u origin $new
   git checkout master
 
 else
- echo "You chose a wrong option.."
+  echo "You chose a wrong option.."
 fi
