9b44bf2e02476ba69c31de82685d5f74a16d2128
priyansh19 <priyansh7533053411@gmail.com>
2019-08-23 23:16:26 +0530

    testing

---
+++ Auto_initialise.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo '====================== Initialising Local Repo ========================='
+echo '1. press 1 to initalise a local repo '
+echo '2. press 2 to clone a repository '
+read xterm
+if [ $xterm -eq '1' ]
+then
+  echo 'Enter the name of local repo you want initialise :'
+  read name
+  cd ~
+  mkdir Onedrive/Desktop/$name
+  cd Onedrive/Desktop/$name
+  git init
+  read link
+  git remote add origin master "$link"
+elif [ $xterm -eq '2']
+then
+  echo 'Enter the link of Github repo:'
+  read link2
+  cd ~
+  cd Onedrive/Desktop/
+  git clone "$link2"
+else
+  echo "You chose a wrong option.."
+fi
